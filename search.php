a130641cc9cee9695bb5e94ff1882d86a7bb8f72
uxian88210 <73234430+uxian88210@users.noreply.github.com>
2021-03-17 23:53:35 +0100

    Update search.php

--- search.php
+++ search.php
@@ -10,21 +10,21 @@
     </div>
 </div>
 <div class="container">
-    <div class="row">
-
         <?php
         if ( have_posts() ) :
         while ( have_posts() ) : the_post();
         ?>
-        <h3 class="text-center"><?php the_title() ?></h3>
-        <?php the_post_thumbnail('medium', array('title' => get_the_title(), 'alt' => get_the_title(), 'class' => 'img-responsive')) ?>
-        <?php the_excerpt() ?>
-        <div class="text-center m-2">
-            <a href="<?php the_permalink() ?>"><button class="btn btn-primary mr-xs mb-sm" type="button">En savoir plus</button></a>
+        <div class="row">
+            <h3 class="text-center"><?php the_title() ?></h3>
+            <?php the_post_thumbnail('medium', array('title' => get_the_title(), 'alt' => get_the_title(), 'class' => 'img-responsive')) ?>
+            <?php the_excerpt() ?>
+            <div class="text-center m-2">
+                <a href="<?php the_permalink() ?>"><button class="btn btn-primary mr-xs mb-sm" type="button">En savoir plus</button></a>
+            </div>
         </div>
-    </div>
     <hr>
     <?php endwhile; else : _e('Aucun résultat ne correspond à votre recherche','agf'); endif; ?>
+
 </div>
 
 
